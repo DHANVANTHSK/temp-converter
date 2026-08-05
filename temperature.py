c3537c29c574f188b712a77b04fb27c4374572b7
RamAbimathan <mathanmoorthy136@gmail.com>
2026-08-05 16:36:15 +0530

    New feature added

--- temperature.py
+++ temperature.py
@@ -1,6 +1,9 @@
 def celsius_to_fahrenheit(c):
     return (c * 9 / 5) + 32
+def fahrenheit_to_celsius(f):
+    return ((f-32)*5/9)
 if __name__ == "__main__":
     c = 25
     f = celsius_to_fahrenheit(c)
     print(f"{c}C is equal to {f}F")
+    print(f"{f}F is equal to {c}C")
